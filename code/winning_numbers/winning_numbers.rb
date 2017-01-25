3a85fb8dbf340bfbf73ae8b5fd263ad836b24edb
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 17:04:12 -0500

    non-include? iteration

--- code/winning_numbers/winning_numbers.rb
+++ code/winning_numbers/winning_numbers.rb
@@ -1,4 +1,19 @@
 def winning_number?(num, array)
 
-	array.include? num
+	
+
+	if array.class == Array
+
+		#array.include? num
+		
+		array.each do |n|
+
+			if n == num
+
+				return true
+			end
+		end
+	end
+
+	false
 end
