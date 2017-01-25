104c902591d2181677b1882c386d3f6c1e45a900
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 16:45:51 -0500

    passes for string in string array

--- code/winning_numbers/test_winning_numbers.rb
+++ code/winning_numbers/test_winning_numbers.rb
@@ -13,8 +13,13 @@ class TestWinningNumbers < Minitest::Test
 		assert_equal(false, winning_number?(3, [1,2]))
 	end
 
-	def test_empty_array
+	def test_empty_array_returns_false
 
 		assert_equal(false, winning_number?(1, []))
+	end
+
+	def test_name_returns_true
+
+		assert_equal(true, winning_number?("Matt", ["Matt", "Max", "Andrew"]))
 	end
 end
