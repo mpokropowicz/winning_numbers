ea9a1a236efac8ecf9aaf7aed23b703ad67fa62a
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 22:26:01 -0500

    test_1234_returns_1235_and_1134 passes

--- code/winning_numbers/one_away_extra.rb
+++ code/winning_numbers/one_away_extra.rb
@@ -6,22 +6,27 @@ def nums_one_away(num, array)
 	numArray = num.to_s.split("")
 	oneOff = []
 
-	print numArray
-	array.each do |n|
+	array.each do |n| #itterate over array element
 
 		index = 0
+		count = 0
 
-		numArray.each do |i|
-
-			print index
-
+		numArray.each do |i| #itterate over each digit in num
 
 			if n[index].to_i == i.to_i + 1 || n[index].to_i == i.to_i - 1
-				oneOff << n
+				count += 1
 			end
+
 			index += 1
 		end
 
+		if count == 1 #only one digit can be one away
+				oneOff << n
+			end
 	end
 
+	oneOff
+
 end
+
+nums_one_away('1234', ['9999', '5678', '1235', '1134', '1344'])
