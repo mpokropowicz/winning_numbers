9c0cc6209d4f6acd845056b2a5891a0858a12481
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 20:45:52 -0500

    test_1234_returns_1235 failed

--- code/winning_numbers/test_one_away_extra.rb
+++ code/winning_numbers/test_one_away_extra.rb
@@ -8,4 +8,8 @@ class TestOneAwayExtra < Minitest::Test
 		assert_equal(false, nums_one_away(1234, []))
 	end
 
+	def test_1234_returns_1235
+
+		assert_equal(1235, nums_one_away(1234, [1235]))
+	end
 end
