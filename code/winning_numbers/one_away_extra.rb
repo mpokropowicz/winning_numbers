9fc91048d6bb631495e8cf0bcc7fa2ce132a9796
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 21:35:29 -0500

    test_1234_returns_1235 passes

--- code/winning_numbers/one_away_extra.rb
+++ code/winning_numbers/one_away_extra.rb
@@ -1,5 +1,27 @@
 def nums_one_away(num, array)
 
-false
+	#we must pass in an Array and it must be populated
+	return false if array.class != Array || array.size == 0
+
+	numArray = num.to_s.split("")
+	oneOff = []
+
+	print numArray
+	array.each do |n|
+
+		index = 0
+
+		numArray.each do |i|
+
+			print index
+
+
+			if n[index].to_i == i.to_i + 1 || n[index].to_i == i.to_i - 1
+				oneOff << n
+			end
+			index += 1
+		end
+
+	end
 
 end
--- code/winning_numbers/test_one_away_extra.rb
+++ code/winning_numbers/test_one_away_extra.rb
@@ -5,11 +5,11 @@ class TestOneAwayExtra < Minitest::Test
 
 	def test_empty_array_returns_false
 
-		assert_equal(false, nums_one_away(1234, []))
+		assert_equal(false, nums_one_away('1234', []))
 	end
 
 	def test_1234_returns_1235
 
-		assert_equal(1235, nums_one_away(1234, [1235]))
+		assert_equal(['1235'], nums_one_away('1234', ['1235']))
 	end
 end
